84e19a3194b46fa1e699fc89f12bb8be72b07080
trignomtry <northruptrig@gmail.com>
2025-06-06 00:12:54 -0400

    f

--- src/utils.rs
+++ src/utils.rs
@@ -212,8 +212,8 @@ pub async fn spin(cfg: &Config, user_js: Vec<(String, Vec<u8>)>) -> std::io::Res
         r#"
     {{
   "boot-source": {{
-    "kernel_image_path": "/mnt/vmlinux",    
-"boot_args": "console=ttyS0 reboot=k panic=1 pci=off root=/dev/vda init=/sbin/init"
+    "kernel_image_path": "/mnt/vmlinux",
+    "boot_args": "console=ttyS0 reboot=k panic=1 pci=off root=/dev/vda init=/sbin/init"
     }},
   "drives": [
     {{
